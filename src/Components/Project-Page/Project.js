a320b592cfd86757854ec11be20f782bbdb1fb78
Fireryred <39081263+Fireryred@users.noreply.github.com>
2024-09-24 00:29:44 +0800

    added projects
    projects + its css

--- src/Components/Project-Page/Project.js
+++ src/Components/Project-Page/Project.js
@@ -1,12 +1,51 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Card } from "react-bootstrap";
-function Project() {
+import Card from "react-bootstrap/Card";
+import BBImg from "../../Images/Projects/BantayBayan.jpg";
+
+function Project(props) {
+  const setProjects = () => {
+    let projects = [];
+    for (let index = 0; index < props.length; index + 2) {
+      projects.push(
+        <Row xs={5} data-bs-theme="dark">
+          <Col />
+          <Col className="ps-5 project-card">
+            <Card>
+              <Card.Img className="project-card-img" src="" />
+            </Card>
+          </Col>
+        </Row>
+      );
+    }
+  };
+  console.log(props);
   return (
-    <Row xs={5}>
-      <Col>
+    <Row xs={5} data-bs-theme="dark">
+      <Col />
+      <Col className="ps-5 project-card">
         <Card>
-          <Card.Img></Card.Img>
+          <Card.Img className="project-card-img" src={BBImg} />
+        </Card>
+        <div className="project-container ">
+          <div className="project-title fs-5">
+            <div>BantayBayan</div>
+            <div className="line" />
+            <div>2017</div>
+          </div>
+          <div className="text-green project-language">
+            Java JSP - MYSql - Bootstrap4 - MailAPI
+          </div>
+          <div className="project-content">
+            A Management Information System, it connects civilians and village
+            official digitally, aiding in submitting and receiving issues within
+            the community.
+          </div>
+        </div>
+      </Col>
+      <Col className="pe-5 project-card">
+        <Card>
+          <Card.Img src={BBImg}></Card.Img>
           <Card.Title></Card.Title>
           <Card.Subtitle></Card.Subtitle>
           <Card.Text></Card.Text>
--- src/Components/Project-Page/Projects.js
+++ src/Components/Project-Page/Projects.js
@@ -1,7 +1,50 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Project from "./Project";
 
+import BantayBayanImage from "../../Images/Projects/BantayBayan.jpg";
+import RealifsImage from "../../Images/Projects/Realifs.jpg";
+import SolobitImage from "../../Images/Projects/SoloBit.png";
+import MyxaImage from "../../Images/Projects/SoloBit.png";
+
+const projects = {
+  0: {
+    name: "Realifs",
+    image: { RealifsImage },
+    language: "React-Native - Firestore - Mongodb - DirectionAPI",
+    description:
+      "Think of Grab/Uber but instead of food, you are delivering donation, " +
+      "It connects donor, delivery persons (fetchers), and donation effort " +
+      "organization (CSO administrators)",
+  },
+  1: {
+    name: "Solobit",
+    image: { SolobitImage },
+    language: "PHP - Java - MySQL - Bootstrap",
+    description:
+      "An e-commerce webisite as a solo project with the goal of me getting " +
+      "used to PHP, then later switched it to Java",
+  },
+  2: {
+    name: "Myxa",
+    image: { MyxaImage },
+    language: "Java - Firestore - MapsAPI - DirectionAPI",
+    description:
+      "Meal kit delivery service, like blue apron, is accesible through you " +
+      "phone. Be able to plan your meals with your phone",
+  },
+  3: {
+    name: "BantayBayan",
+    image: { BantayBayanImage },
+    language: "Java JSP - MYSql - Bootstrap4 - MailAPI",
+    description:
+      "A Management Information System, it connects civilians and village " +
+      "official digitally, aiding in submitting and receiving issues within " +
+      "the community.",
+  },
+};
+const projectLength = Object.keys(projects).length;
 function Projects() {
   return (
     <section>
@@ -9,14 +52,15 @@ function Projects() {
         <Row xs={5}>
           <Col />
           <Col xs={7} className="px-5">
-            <div className="text-end project-title mb-3">
+            <div className="text-end heading-title mb-3">
               <div className="line" />
               <div>
-                Projects<span className="text-success">.</span>
+                Projects<span className="text-green">.</span>
               </div>
             </div>
           </Col>
         </Row>
+        <Project projects={projects} length={projectLength} />
       </Container>
     </section>
   );
