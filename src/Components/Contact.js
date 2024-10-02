e9b833d8549ae2837730e30dc16a56a19e0edab3
Fireryred <39081263+Fireryred@users.noreply.github.com>
2024-10-03 00:59:15 +0800

    added contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,61 @@
-function Contact() {
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import emailImage from "../Images/SVG/email.svg";
+
+function Contact(props) {
   return (
-    <section>
-      <Container fluid className="sub-container" id="contact">
-        <div className="ref">
-          <div
-            ref={(div) => {
-              props.handleRef(div, 0);
-            }}
-          />
-        </div>
-        <Row xs={5}>
-          <Col></Col>
-        </Row>
-      </Container>
-    </section>
+    <Container fluid className="sub-container" id="contact">
+      <div className="ref">
+        <div
+          ref={(div) => {
+            props.handleRef(div, 3);
+          }}
+        />
+      </div>
+      <Row xs={5}>
+        <Col />
+        <Col xs={7} className="px-5 contact">
+          <p className="contact-title">
+            Contact<span className="text-green">.</span>
+          </p>
+          <p className="contact-content">
+            Email me if you want to connect! You can also find me at
+            <br />
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/gershom-gruta/"
+              className="text-green"
+            >
+              linkedin
+            </a>
+            &nbsp;or&nbsp;
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.jobstreet.com.ph/profile/gershom-gruta-YgYM0KhBRz"
+              className="text-green"
+            >
+              jobstreet.
+            </a>
+          </p>
+          <div className="contact-footer">
+            <a
+              className="contact-email"
+              target="_blank"
+              rel="noreferrer"
+              href="mailto:gershomgruta06@gmail.com"
+            >
+              <img alt="" src={emailImage} />
+              <span className="text-white">gershomgruta06@gmail.com</span>
+            </a>
+          </div>
+        </Col>
+        <Col />
+      </Row>
+    </Container>
   );
 }
 
