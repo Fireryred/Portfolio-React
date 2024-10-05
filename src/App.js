5086f755a0095b7c53c4beadb49a7b4bdab57d57
Shion Bluu <30545387+Shionbluu@users.noreply.github.com>
2024-10-05 14:23:03 +0800

    change title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import Index from "./Components";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "Gershom Gruta | Software Engineer";
+  }, []);
   return <Index />;
 }
 
